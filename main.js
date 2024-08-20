77f850e8fca29c5e4e8c7e312c61bc14485cca2d
chriswebdv <chriswebdv@gmail.com>
2024-08-20 16:57:05 -0600

    create 2 variables, empty array and inputEl

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const myLeads = [];
+const inputEl = document.querySelector(".inputEl");
+
 const inputBtn = document.querySelector(".inputBtn");
 inputBtn.addEventListener("click", () => {
   console.log("Button Clicked");
