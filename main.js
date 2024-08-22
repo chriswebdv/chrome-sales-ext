1f7627fbdbc803f800799c61ada45df00fbf315a
chriswebdv <chriswebdv@gmail.com>
2024-08-22 16:44:47 -0600

    create ul list and grab it from the DOM

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-const myLeads = [];
+const myLeads = [
+  "www.awesomelead.com",
+  "www.epiclead.com",
+  "www.greatlead.com",
+];
 const inputEl = document.querySelector(".inputEl");
-
 const inputBtn = document.querySelector(".inputBtn");
+const ulEl = document.querySelector(".ulEl");
+
 inputBtn.addEventListener("click", () => {
-  myLeads.push("www.awesomelead.com");
+  myLeads.push(inputEl.value);
   console.log(myLeads);
+  inputEl.value = "";
 });
+
+for (let i = 0; i < myLeads.length; i++) {
+  console.log(myLeads[i]);
+}
