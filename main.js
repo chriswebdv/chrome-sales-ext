66c3e4b468fa42201d28d9951bd4fab105feb29a
chriswebdv <chriswebdv@gmail.com>
2024-08-22 17:45:37 -0600

    working on refactoring function

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
-const myLeads = [
-  "www.awesomelead.com",
-  "www.epiclead.com",
-  "www.greatlead.com",
-];
+const myLeads = [];
 const inputEl = document.querySelector(".inputEl");
 const inputBtn = document.querySelector(".inputBtn");
 const ulEl = document.querySelector(".ulEl");
 
 inputBtn.addEventListener("click", () => {
   myLeads.push(inputEl.value);
-  console.log(myLeads);
+  renderLeads();
   inputEl.value = "";
 });
 
-for (let i = 0; i < myLeads.length; i++) {
-  //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
-  const li = document.createElement("li");
-  li.textContent = myLeads[i];
-  ulEl.append(li);
+function renderLeads() {
+  for (let i = 0; i < myLeads.length; i++) {
+    //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+    let li = document.createElement("li");
+    li.textContent = myLeads[i];
+    ulEl.append(li);
+  }
 }
